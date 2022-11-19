6dd347bcfa1713b369e2a86ee53faa9cab12a448
Oleksiy Stepaniuk <37981-Shdulin@users.noreply.drupalcode.org>
2022-11-19 12:56:26 +0100

    Issue #3055810 by Oleksiy: Wrong field edited when multiple entities are rendered on page

--- js/quicker_edit.js
+++ js/quicker_edit.js
@@ -5,20 +5,13 @@
    */
   Drupal.behaviors.quickerEditTrigger = {
     attach: function (context, settings) {
-      $('[data-quickedit-field-id]').on('dblclick', function () {
-        var $fieldElement = $(this);
-
-        // Fetch the quickedit field id of the clicked element.
-        var fieldID = $fieldElement.data('quickedit-field-id');
-
-        // Extract the entity id.
-        var entityID = fieldID.split('/').slice(0, 2).join('/');
+      $('[data-quickedit-field-id]', context).on('dblclick', function () {
+        let $fieldElement = $(this);
 
         // Find the entity element.
-        var $entityElement = $('[data-quickedit-entity-id="' + entityID + '"]');
+        let $entityElement = $fieldElement.closest('[data-quickedit-entity-id]');
 
         // Enable the Quick Edit by programatically triggering the contextual link.
-        $entityElement.find('[data-contextual-id] > button').trigger('click');
         $entityElement.find('[data-contextual-id] .quickedit > a').trigger('click');
 
         // Programmatically trigger a click once again on the field, to start
